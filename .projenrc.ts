61c62801e5dfd4c095d8b734da0c6d0cf96b24cb
Roman Filippov <rf@romanfilippov.com>
2023-01-09 10:35:53 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,12 @@
+import { awscdk, javascript } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Roman Filippov',
+  authorAddress: 'rf@romanfilippov.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'aws-cdk-repro-23596',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/rf/aws-cdk-repro-23596.git',
+});
+project.synth();
